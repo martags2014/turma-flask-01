810a6ba5ab59985b5a12c59421336dab1f330a5f
MartaGS <martags2014@outlook.com>
2026-05-07 19:49:58 -0300

    Criação da Classe Aluno

---
+++ Aluno.py
@@ -0,0 +1,10 @@
+class Aluno():
+
+    def __init__(self, nome, email, senha, telefone):
+        self.nome = nome
+        self.senha = senha
+        self.email = email
+        self.telefone = telefone
+
+    def login(self, email, senha):
+        print('Login realizado com sucesso!')
