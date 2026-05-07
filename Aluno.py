39cca7617542491ba9dc7e9a4b712d1f824028fd
MartaGS <martags2014@outlook.com>
2026-05-07 20:31:46 -0300

    Inclusão do atributo Data de Nascimento

--- Aluno.py
+++ Aluno.py
@@ -1,10 +1,11 @@
 class Aluno():
 
-    def __init__(self, nome, email, senha, telefone):
+    def __init__(self, nome, email, senha, telefone, data_nasc):
         self.nome = nome
         self.senha = senha
         self.email = email
         self.telefone = telefone
+        self.data_nasc = data_nasc
 
     def login(self, email, senha):
         print('Login realizado com sucesso!')
--- Script.sql
+++ Script.sql
@@ -8,4 +8,5 @@ id BIGINT AUTO_INCREMENT PRIMARY KEY,
 nome varchar(150) not null,
 email varchar(255) not null UNIQUE,
 senha varchar(19) not null,
-telefone varchar(14));
+telefone varchar(14),
+data_nasc datetime);
